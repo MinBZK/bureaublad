536028e4a13b287d8bd5afa4d777163bd0958dc0
Eric Wout van der Steen <ericwout.steen@rijksoverheid.nl>
2025-03-26 11:30:04 +0100

    API url extra prefix gegeven

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,7 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=("dev.env", ".env", "prod.env"), extra="ignore")
-    API_V1_STR: str = "/v1"
+    API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
 
     DEBUG: bool = False
