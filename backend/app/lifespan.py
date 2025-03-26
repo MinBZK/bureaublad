702cf297104c48769818674daff9376bc3cbb5dd
Eric Wout van der Steen <ericwout.steen@rijksoverheid.nl>
2025-03-26 11:13:56 +0100

    Lint-errors gefixt

--- backend/app/lifespan.py
+++ backend/app/lifespan.py
@@ -4,15 +4,12 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.config import settings
-
 logger = logging.getLogger(__name__)
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    logger.info(f"Starting version 0.1.0")
+    logger.info("Starting version 0.1.0")
     yield
 
-    logger.info(f"Stopping application version 0.1.0")
+    logger.info("Stopping application version 0.1.0")
     logging.shutdown()
