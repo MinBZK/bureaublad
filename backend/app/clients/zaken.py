7357731d209f1fe68406a04662a98c3fdc93aeb5
Eric Wout van der Steen <ericwout.steen@rijksoverheid.nl>
2025-04-14 09:06:11 +0200

    Formatting

--- backend/app/clients/zaken.py
+++ backend/app/clients/zaken.py
@@ -13,9 +13,7 @@ class ZakenClient:
         self.user_id = user_id
         self.user_representation = user_representation
 
-    def get_zaken(
-            self, path: str = "zaken/api/v1/zaken", page: int = 1
-    ) -> list[Zaak]:
+    def get_zaken(self, path: str = "zaken/api/v1/zaken", page: int = 1) -> list[Zaak]:
         payload = {
             "iss": settings.OPENZAAK_CLIENT_ID,
             "iat": int(time.time()),  # current time in seconds
@@ -26,11 +24,14 @@ class ZakenClient:
         jwt_token = jwt.encode(payload, settings.OPENZAAK_SECRET, algorithm=ALGORITHMS.HS256)
 
         client = httpx.Client(
-            headers={"Authorization": f"Bearer {jwt_token}", "Content-Type": "application/json", "Accept-Crs": "EPSG:4326"}
+            headers={
+                "Authorization": f"Bearer {jwt_token}",
+                "Content-Type": "application/json",
+                "Accept-Crs": "EPSG:4326",
+            }
         )
 
         url = httpx.URL(f"{self.base_url}/{path}", params={"page": page, "ordering": "startdatum"})
-
 
         response = client.request("GET", url)
         if response.status_code != 200:
