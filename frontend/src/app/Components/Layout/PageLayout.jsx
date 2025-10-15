cb17c09967cc93f024529d80af5dbfe24859c5b3
abimourad <abi.mourad@rijksoverheid.nl>
2025-10-15 13:20:38 +0200

    ✨(frontend) add 401 page

--- frontend/src/app/Components/Layout/PageLayout.jsx
+++ frontend/src/app/Components/Layout/PageLayout.jsx
@@ -30,6 +30,21 @@ export default function PageLayout({ children }) {
     fetchProfile();
   }, []);
 
+  useEffect(() => {
+    setLoading(true);
+    const fetchProfile = async () => {
+      try {
+        const res = await axios.get(baseUrl + "/api/v1/ocs/activities");
+        setProfile(res.data);
+      } catch (err) {
+        console.error(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProfile();
+  }, []);
+
   return (
     <Layout>
       <HeaderLayout isProfile={!!error} />
