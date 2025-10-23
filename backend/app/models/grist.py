cd7d7efa8fc0d09cd8388f0268c44c815b49bb86
ChristopherSpelt <christopherspelt@icloud.com>
2025-10-23 13:49:57 +0200

    ✨(backend) add grist documents api endpoint
    ✨(backend) add grist documents api endpoint

--- backend/app/models/grist.py
+++ backend/app/models/grist.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, computed_field
 from pydantic.alias_generators import to_camel
+
+from app.core.config import settings
 
 
 class GristOrganization(BaseModel):
@@ -8,8 +10,42 @@ class GristOrganization(BaseModel):
         validate_by_alias=True,
         alias_generator=to_camel,
     )
+    id: int
     name: str
     domain: str | None
     access: str
     created_at: str
     updated_at: str
+
+
+class GristDocument(BaseModel):
+    model_config = ConfigDict(
+        validate_by_name=True,
+        validate_by_alias=True,
+        alias_generator=to_camel,
+    )
+    id: str
+    name: str
+    access: str
+    is_pinned: bool
+    url_id: str | None
+    created_at: str
+    updated_at: str
+
+    @computed_field
+    @property
+    def url(self) -> str:
+        return f"{settings.GRIST_URL}/{self.url_id}/"
+
+
+class GristWorkspace(BaseModel):
+    model_config = ConfigDict(
+        validate_by_name=True,
+        validate_by_alias=True,
+        alias_generator=to_camel,
+    )
+    id: int
+    name: str
+    access: str
+    org_domain: str
+    docs: list[GristDocument]
